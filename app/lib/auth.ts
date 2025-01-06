861985d3f367890fe8c51794cde9a1c74a4a202f
wingie125 <arbab.vns123@gmail.com>
2025-01-06 21:41:32 +0530

    Auth fix

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import prisma from "./db";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
 
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
